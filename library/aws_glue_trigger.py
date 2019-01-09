e0c5cdb50b7c2cebb5353890b0618d8404c18360
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-01-09 05:25:37 +0000

    Add type

--- library/aws_glue_trigger.py
+++ library/aws_glue_trigger.py
@@ -10,21 +10,45 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 from ansible.module_utils.aws.core import AnsibleAWSModule
 from ansible.module_utils.ec2 import camel_dict_to_snake_dict
 
-# Non-ansible imports
-import copy
 try:
     from botocore.exceptions import BotoCoreError, ClientError
 except ImportError:
     pass
 
 
-def create_or_update_glue_trigger(client, module):
+def _is_params_equal(user_params, current_params):
+    return True
+
+
+def _get_glue_trigger(client, name):
+    try:
+        return client.get_trigger(Name=name)['Trigger']
+    except (BotoCoreError, ClientError) as e:
+        if e.response['Error']['Code'] == 'EntityNotFoundException':
+            return None
+        else:
+            raise e
+
+
+def create_or_update_glue_trigger(client, params):
+
+    user_params = dict()
+    if _get_glue_trigger(client, params.get('name')) is None:
+        # Create
+        return True
+
+    current_params = dict()
+    if _is_params_equal(user_params, current_params):
+        return False
+
+    # Update
+    return True
+
+
+def delete_glue_trigger(client, params):
     changed = False
     return changed
 
-def delete_glue_trigger(client, module):
-    changed = False
-    return changed
 
 def main():
 
@@ -33,13 +57,18 @@ def main():
             state=dict(require=False, type='str', default='present',
                        choices=['present', 'absent']),
             name=dict(required=True, type='str'),
+            trigger_type=dict(required=True, type='str',
+                              choice=['SCHEDULED', 'CONDITIONAL', 'ON_DEMAND']),
+            schedule=dict(required=False, type='str'),
+            actions=dict(required=True, type='list'),
         )
     )
 
-    module = AnsibleAWSModule(argument_spec=argument_spec
-                              # required_if=[
-                            #       ('state', 'present', ['role', 'command_script_location'])
-                            # ]
+    module = AnsibleAWSModule(
+        argument_spec=argument_spec,
+        required_if=[
+            ('trigger_type', 'SCHEDULED', ['schedule'])
+        ]
     )
 
     client = module.client('glue')
